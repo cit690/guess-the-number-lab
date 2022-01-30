d3aab71b3dc874de63b7d17db0ea3157f247f5ad
Claire Thompson <clairethompson@Claires-MacBook-Pro.local>
2022-01-30 11:00:42 -0500

    edits

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const prevGuesses = [] 
+const prevGuesses = [];
 
 
 const game = {
@@ -11,3 +11,19 @@ const game = {
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
   }
 }
+
+ function getGuess(){
+
+ }
+
+ prevGuesses.push()
+
+//prompt('Guess a number between 1-100')
+
+// if(guess < secretNum){
+//   return alert('Guessed too low!')
+// } else if(guess > secretNum){
+//   return alert('Guessed too high!')
+// } else if(guess === secretNum){
+//   return alert('Congrats! You did it! You made ' + prevGuesses.length + 'guesses!')
+// }
