2f6989692de22d9df90aaec2f0e82b45ee90babf
Claire Thompson <clairethompson@Claires-MacBook-Pro.local>
2022-01-30 12:45:13 -0500

    a little cleaner now

--- app.js
+++ app.js
@@ -6,27 +6,28 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
+  guess: parseInt(prompt(`Guess a number between ${this.smallestNum} and ${this.biggestNum}`)),
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-      function getGuess(){
-        let guess = parseInt(prompt(`Guess a number between ${this.smallestNum} and ${this.biggestNum}`)
-        for(let i = 0; i <= 100; i ++)
-      }
-      if(guess < secretNum){
-        return alert('Guessed too low!')
-         prevGuesses.push
-      } else if(guess > secretNum){
-        return alert('Guessed too high!')
-         prevGuesses.push
-      } else if(guess === secretNum){
-        return alert('Congrats! You did it! You made ' + prevGuesses.length + 'guesses!')
-      } else {null}
-  }
-
+      
+  } ,
+  getGuess: function(){
+  for(let i = 0; i <= 100; i ++){
+  
+  if(guess < secretNum){
+    return alert('Guessed too low!')
+     prevGuesses.push
+  } else if(guess > secretNum){
+    return alert('Guessed too high!')
+     prevGuesses.push
+  } else if(guess === secretNum){
+    return alert('Congrats! You did it! You made ' + prevGuesses.length + 'guesses!')
+  } else {null}
 }
+    }
 
-
+  }
 
 
 
