43349e6f1fc0a7dbac44caa7cbca6b4347eb482d
Claire Thompson <clairethompson@Claires-MacBook-Pro.local>
2022-01-31 09:41:59 -0500

    maybe actually something?

--- app.js
+++ app.js
@@ -6,45 +6,44 @@ const game = {
   smallestNum: 1,
   secretNum: null,
   prevGuesses: [],
+  play: function() {
+    this.secretNum = Math.floor(Math.random() * 
+      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+    do {
+      this.prevGuesses.push(this.getGuess())
+      console.log(this.prevGuesses)
+      console.log(this.render)
+    } while (this.prevGuesses[this.prevGuesses.length -1] !== this.secretNum) 
+      
+  } ,
   
   getGuess: function(){
     let guess ;
     do{
     guess = parseInt(prompt(`Guess a number between ${this.smallestNum} and ${this.biggestNum}`))
-    }
-    while (isNaN(guess) || guess < this.smallestNum || guess > this.biggestNum) 
+    } while (isNaN(guess) || guess < this.smallestNum || guess > this.biggestNum) 
     
     return guess
 
+  },
+
+  render: function(){
+    if(guess < this.secretNum){
+            return alert('Guessed too low!')
+             prevGuesses.push
+          } else if(guess > this.secretNum){
+            return alert('Guessed too high!')
+             this.prevGuesses.push
+          } else if(guess === this.secretNum){
+            return alert('Congrats! You did it! You made ' + this.prevGuesses.length + 'guesses!')
+          } else {null}
+    
   }
 
-,
-  play: function() {
-    while (this.getGuess !== this.secretNum) {
-      return this.prevGuesses.push(this.getGuess())
-      
-      
-    }
-
-    this.secretNum = Math.floor(Math.random() * 
-      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-      
-  } ,
-  
-
-  }
+}
 
  
-console.log(game.getGuess())
-// if(guess < this.secretNum){
-      //   return alert('Guessed too low!')
-      //    prevGuesses.push
-      // } else if(guess > this.secretNum){
-      //   return alert('Guessed too high!')
-      //    this.prevGuesses.push
-      // } else if(guess === this.secretNum){
-      //   return alert('Congrats! You did it! You made ' + prevGuesses.length + 'guesses!')
-      // } else {null}
+ //game.play()
 
 
 
